ed2586fe9d827fd8f7de60ac46e27d0b0039deed
Sergio <uo276341@uniovi.es>
2023-11-18 15:51:14 +0100

    Fix random seed for reproducibility

--- simulacion.py
+++ simulacion.py
@@ -1,6 +1,7 @@
 import simpy
 import random
-import numpy as np
+
+random.seed(42)  # Semilla para reproducibilidad
 
 
 class Puerto:
@@ -9,51 +10,28 @@ class Puerto:
         self.remolcadores = simpy.Resource(env, capacity=num_remolcadores)
         self.muelles = simpy.Resource(env, capacity=num_muelles)
         self.tiempo_descarga = lambda: random.chisquare(
-            5
+            5 * 60
         )  # Chi-cuadrado con 5 grados de libertad para tiempo de descarga
         self.puntos_llegada = [
-            (x * 60, y) for x, y in puntos_llegada
+            (x * 60, y) for x, y in puntos_llegada.items()
         ]  # Convertir las coordenadas x a minutos
         self.tiempos_atraque = []  # Lista para almacenar los tiempos de atraco
         self.numero_total_barcos_atracados = 0
         self.numero_total_barcos_esperando = 0
         self.cola_espera = simpy.Store(env)
 
-    def llegada_petrolero(self, petrolero):
+    def llegada_petrolero(self):
         tiempo_llegada = self.calcular_tiempo_atraque(llevando_barco=False)
         yield self.env.timeout(tiempo_llegada)
 
         # Intentar obtener un remolcador
         with self.remolcadores.request() as req:
-            remolcador_disponible = yield req
+            yield req
 
-            if remolcador_disponible:  # Si hay un remolcador disponible
-                tiempo_atraque = self.calcular_tiempo_atraque(llevando_barco=True)
-                yield self.env.timeout(tiempo_atraque)
-                self.tiempos_atraque.append(self.env.now)
-                self.numero_total_barcos_atracados += 1
-            else:  # Si no hay remolcador disponible, añadir a la cola de espera
-                self.numero_total_barcos_esperando += 1
-                yield self.cola_espera.put(petrolero)
-                # Esperar a que un remolcador esté disponible
-                remolcador_disponible = yield self.remolcadores.request()
-                tiempo_atraque = self.calcular_tiempo_atraque(llevando_barco=True)
-                yield self.env.timeout(tiempo_atraque)
-                self.tiempos_atraque.append(self.env.now)
-                self.numero_total_barcos_atracados += 1
-
-    def calcular_tiempo_llegada(self):
-        x_actual = self.env.now
-        for i in range(len(self.puntos_llegada) - 1):
-            x0, y0 = self.puntos_llegada[i]
-            x1, y1 = self.puntos_llegada[i + 1]
-            if x_actual >= x0 and x_actual < x1:
-                m = (y1 - y0) / (x1 - x0)
-                b = y0 - m * x0
-                tiempo_llegada = max(
-                    0, (x1 - x_actual) / m
-                )  # Evitar tiempos de llegada negativos
-                return tiempo_llegada
+            tiempo_atraque = self.calcular_tiempo_atraque(llevando_barco=True)
+            yield self.env.timeout(tiempo_atraque)
+            self.tiempos_atraque.append(self.env.now)
+            self.numero_total_barcos_atracados += 1
 
     def calcular_tiempo_atraque(self, llevando_barco):
         if llevando_barco:
@@ -67,22 +45,19 @@ class Puerto:
 # Configuración inicial
 num_remolcadores = 10
 num_muelles = 20
-puntos_llegada = [(0, 5), (5, 7), (8, 6), (15, 9), (17, 6), (24, 5)]
-desplazamientos_file = "./desplazamiento/E5.desplazamientos.txt"
-
+puntos_llegada = {"0": 5, "5": 7, "8": 6, "15": 9, "17": 6, "24": 5}
 # Simulación
 env = simpy.Environment()
-puerto = Puerto(
-    env, num_remolcadores, num_muelles, puntos_llegada, desplazamientos_file
-)
+puerto = Puerto(env, num_remolcadores, num_muelles, puntos_llegada)
 
 
 # Proceso de llegada de petroleros
 def llegada_petroleros(env, puerto):
-    for i in range(10):  # Número de petroleros a simular
-        petrolero = f"Petrolero-{i}"
-        env.process(puerto.llegada_petrolero(petrolero))
-        yield env.timeout(60)  # Intervalo entre llegadas en minutos
+    for i in range(25):
+        if str(i) in puntos_llegada.keys():
+            for _ in range(puntos_llegada[str(i)]):
+                env.process(puerto.llegada_petrolero())
+        # yield env.timeout(60)
 
 
 env.process(llegada_petroleros(env, puerto))
